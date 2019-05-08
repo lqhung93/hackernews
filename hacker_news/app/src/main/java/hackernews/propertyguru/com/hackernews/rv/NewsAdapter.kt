0c898d96446ed47c819a1f50f2d31ddaafd6be9f
Hung Le <hung@huteego.com>
2019-05-08 13:11:58 +0700

    added logic for news adapter

--- hacker_news/app/src/main/java/hackernews/propertyguru/com/hackernews/rv/NewsAdapter.kt
+++ hacker_news/app/src/main/java/hackernews/propertyguru/com/hackernews/rv/NewsAdapter.kt
@@ -1,7 +1,35 @@
 package hackernews.propertyguru.com.hackernews.rv
 
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import hackernews.propertyguru.com.hackernews.R
+import hackernews.propertyguru.com.hackernews.network.responses.GetStoryDetailResponse
+
 /**
  * Created by hung on 5/8/19.
  */
-class NewsAdapter {
+class NewsAdapter(private val storyDetails: List<GetStoryDetailResponse>) : RecyclerView.Adapter<NewsAdapter.RowHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RowHolder {
+        return RowHolder(LayoutInflater.from(parent.context).inflate(R.layout.row_news, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return storyDetails.size
+    }
+
+    override fun onBindViewHolder(holder: RowHolder, position: Int) {
+        holder.bindData(storyDetails[position])
+    }
+
+    class RowHolder(v: View) : RecyclerView.ViewHolder(v) {
+        private val titleTv: TextView = v.findViewById(R.id.title_tv)
+
+        fun bindData(storyDetail: GetStoryDetailResponse) {
+            titleTv.text = storyDetail.title
+        }
+    }
 }
