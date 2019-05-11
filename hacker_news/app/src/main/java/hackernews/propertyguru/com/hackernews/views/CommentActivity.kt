861335da32ef9bc3d3696f3aba5bb3b3b83f0d05
Hung Le <hung@huteego.com>
2019-05-12 00:38:51 +0700

    use stack to show all comments

--- hacker_news/app/src/main/java/hackernews/propertyguru/com/hackernews/views/CommentActivity.kt
+++ hacker_news/app/src/main/java/hackernews/propertyguru/com/hackernews/views/CommentActivity.kt
@@ -13,6 +13,7 @@ import hackernews.propertyguru.com.hackernews.utils.C
 import hackernews.propertyguru.com.hackernews.utils.LogUtils
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
+import java.util.*
 
 class CommentActivity : BaseActivity() {
 
@@ -23,9 +24,11 @@ class CommentActivity : BaseActivity() {
     private var storyDetails: ArrayList<GetStoryDetailResponse> = arrayListOf()
     private var commentsAdapter = CommentsAdapter(storyDetails)
 
+    private var idsStack: Stack<String> = Stack()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_comment)
 
         val linearLayoutManager = LinearLayoutManager(this)
 
@@ -47,11 +50,14 @@ class CommentActivity : BaseActivity() {
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     fun onGettingStoryDetail(response: GetStoryDetailResponse) {
-        LogUtils.e(TAG, "Res: $response")
+        LogUtils.e(TAG, "Comment detail: $response")
 
-        if (TextUtils.isEmpty(response.text)) {
+        if (TextUtils.isEmpty(response.type) || (response.type != "comment")) {
+            getCommentDetail()
+            return
+        }
 
-        } else {
+        if (!TextUtils.isEmpty(response.text)) {
             var index = storyDetails.indexOf(response)
             if (index != -1) {
                 storyDetails[index] = response
@@ -63,14 +69,30 @@ class CommentActivity : BaseActivity() {
             commentsAdapter.notifyItemChanged(index)
         }
 
+        val kids = response.kids
+        if (kids != null && kids.isNotEmpty()) {
+            idsStack.addAll(kids)
+        }
+
+        getCommentDetail()
+
         commentsRefreshLayout?.isRefreshing = false
     }
 
     private fun invokeApis() {
         storyDetails.clear()
 
-        (intent.getSerializableExtra(C.COMMENT_LIST) as ArrayList<String>).forEach {
-            pollingCenter.getStoryDetail(it)
+        // Already check null and last activity
+        idsStack.addAll(intent.getSerializableExtra(C.COMMENT_LIST) as ArrayList<String>)
+
+        getCommentDetail()
+    }
+
+    private fun getCommentDetail() {
+        try {
+            pollingCenter.getStoryDetail(idsStack.pop())
+        } catch (e: EmptyStackException) {
+
         }
     }
 }
