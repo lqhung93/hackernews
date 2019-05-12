21b3708fd37580e4ca40a9fcb706e09ee321e092
Hung Le <hung@huteego.com>
2019-05-12 12:20:06 +0700

    added to string

--- hacker_news/app/src/main/java/hackernews/propertyguru/com/hackernews/network/responses/GetTopStoriesResponse.kt
+++ hacker_news/app/src/main/java/hackernews/propertyguru/com/hackernews/network/responses/GetTopStoriesResponse.kt
@@ -3,4 +3,9 @@ package hackernews.propertyguru.com.hackernews.network.responses
 /**
  * Created by hung on 5/8/19.
  */
-class GetTopStoriesResponse(val ids: ArrayList<String>)
+class GetTopStoriesResponse(val ids: ArrayList<String>) {
+
+    override fun toString(): String {
+        return "GetTopStoriesResponse(ids=$ids)"
+    }
+}
